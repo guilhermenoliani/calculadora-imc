fc29a078023895acc07a02488713156b46a37cf0
Guilherme <guilhermenoliani@gmail.com>
2022-07-06 15:34:11 -0300

    ✅ Criando GridItem e estilizando

--- src/components/GridItem/GridItemStyles.tsx
+++ src/components/GridItem/GridItemStyles.tsx
@@ -1,1 +1,36 @@
 import styled from "styled-components";
+
+export const Main = styled.div`
+  display: flex;
+  justify-content: center;
+  align-itens: center;
+  flex-direction: column;
+  flex: 1;
+  color: #fff;
+  border-radius: 10px;
+  padding: 20px;
+`;
+
+export const gridIcon = styled.div`
+  width: 70px;
+  height: 70px;
+  border-radius: 50%;
+  background-color: rgba(0, 0, 0, 0.1);
+  display: flex;
+  justify-content: center;
+  align-itens: center;
+  margin: auto;
+`;
+
+export const gridTitle = styled.div`
+  text-align: center;
+  font-size: 23px;
+  font-weight: bold;
+  margin-top: 5px;
+`;
+
+export const gridInfo = styled.div`
+  text-align: center;
+  font-size: 12px;
+  margin-top: 15px;
+`;
