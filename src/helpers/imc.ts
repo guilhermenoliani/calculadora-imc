eb3d4850767285c93674af99b20008902f4b69db
Guilherme <guilhermenoliani@gmail.com>
2022-07-07 09:49:35 -0300

    � Resolvendo bug

--- src/helpers/imc.ts
+++ src/helpers/imc.ts
@@ -7,8 +7,8 @@ export type Level = {
 };
 
 export const levels: Level[] = [
-  { title: "Magreza", color: "#96a3ab", icon: "down", imc: [0, 18.5] },
-  { title: "Normal", color: "#0ead69", icon: "up", imc: [18.6, 24.9] },
+  { title: "Magreza", color: "#96a3ab", icon: "down", imc: [0, 18.59] },
+  { title: "Normal", color: "#0ead69", icon: "up", imc: [18.6, 24.99] },
   { title: "Sobrepeso", color: "#e2b036", icon: "down", imc: [25, 30] },
   { title: "Obesidade", color: "#c3423f", icon: "down", imc: [30.1, 99] },
 ];
